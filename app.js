ab9364ec261f34fbbdec5584fc1416e68bdaeb85
Matt <matt-paul@live.com>
2020-09-22 21:46:48 -0400

    JS changes: -filled in the initialize func and set all the h1s from teh resutls to empty -updated the operator click handler to check the operatorion, perform math calcu and render -update the equal method to handle the calc -updated the clear to call init function

--- app.js
+++ app.js
@@ -1,10 +1,12 @@
 // flow of app
 /**
+ * initialize the calcultaotr
  * have a first number
  * choose an operator
  * then a second number
  * wait for operation to finish
  * display the results
+ * clear old result to redo
  */
 
 
@@ -15,11 +17,11 @@ $( document ).ready(function() {
 // Variables in global runtime
 
 //initial state
-let firstNumber = 0
-let secondNumber = 0
+let firstNumber = ''
+let secondNumber = ''
 let result = 0   
 let operator = ''
-let isOperatorChosen = true
+let isOperatorChosen = false
 let isCalculated = false
 
 
@@ -27,6 +29,15 @@ let isCalculated = false
 // 1 Start calculator initialization
 // 2 when someone hits clear, reset calculator
 function initializeCalculator() {
+    firstNumber = ''
+    secondNumber = ''
+    result = 0   
+    operator = ''
+    isOperatorChosen = false
+    isCalculated = false
+
+//update the ui
+$('#first-number, #second-number, #operator, #result').empty()
     //setup initial state of data so user can go through flow
 }
 
@@ -38,24 +49,86 @@ $('.number').on('click', function(e) {
     // check if operation is chosen
     if (isOperatorChosen) {
             // if so, should be writing the second number
-        console.log($(this).val())
+       
+       secondNumber += $(this).val()
+       $('#second-number').text(secondNumber)
     } else {
             //otherwise we should be rendering the first number
-        console.log($(this).val())
+       firstNumber += $(this).val()
+       $('#first-number').text(firstNumber)
     }
 
 })
 
 $('.equal').on('click', function(e) {
-    console.log(this)
+
+
+    /**
+     * flow:
+     * 1. has the calculation on current number finished?
+     * a. if yes, do nothing
+     * b. if no, then going to set isCalculated to true
+     * 2. convert the numbers from strings to numbers to calculate
+     * 3. check what operator the user has chosen 
+     * 4. perform the operation on the two numbers adn save to result
+     * 5. render the result
+     */
+    //1a
+    if(isCalculated) {
+        return false
+    }
+    //1-b
+    isCalculated = true
+
+    //2
+    firstNumber = parseInt(firstNumber)
+    secondNumber = parseInt(secondNumber)
+
+
+    //3
+    switch(operator) {
+        case 'plus':
+            result = firstNumber + secondNumber          
+            break
+        case 'minus':
+            result = firstNumber - secondNumber
+            break
+        case 'times':
+            result = firstNumber * secondNumber
+            break
+        case 'divide':
+            result = firstNumber / secondNumber
+            break
+        case 'power':
+            result = firstNumber ** secondNumber
+            break
+        default:
+            operator
+            break
+    }
+    //5
+    $('#result').text(result)
 })
 
 $('.operator').on('click', function(e) {
-    console.log(this)
+    // check if weve already ran a calculation or first number has been chosen
+    // we dont want to do anything
+    if(!firstNumber || isCalculated) {
+        return false
+    }
+    console.log($(this).val())
+
+    //set isOperatorChosen to true so we can start choosing the second number
+    isOperatorChosen = true
+    //save value of operator user has clicked on
+    operator = $(this).val()
+    //render that to the operator result
+    $('#operator').text(operator)
 })
 
 $('.clear').on('click', function(e) {
-    console.log(this)
+    // call initialized calculator, whole point of function  is to setup the initial state
+    initializeCalculator()
 })
 
 // run th app
